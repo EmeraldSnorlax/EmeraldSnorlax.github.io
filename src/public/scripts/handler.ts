f838c8f8e7456d985a060ca6dde3f3e34f669ea5
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-03 09:43:08 +0000

    clicking send button, clear prompt on send

--- src/public/scripts/handler.ts
+++ src/public/scripts/handler.ts
@@ -1,10 +1,13 @@
 import help from './commands/help'; 
 import { write } from './line';
+import { prompt } from './main';
 export default function handler(rawinput: string): void {
+	(prompt as HTMLInputElement).value = '';
 	const args = rawinput.split(/[ ]+/).shift();
 	const command = rawinput.split(/[ ]+/)[0];
 	switch(command) {
 	case 'help':
 		help();
+	
 	}
 }
--- src/public/scripts/main.ts
+++ src/public/scripts/main.ts
@@ -4,9 +4,14 @@ import init from './init';
 import handler from './handler';
 init();
 
-const prompt = document.getElementById('prompt');
+export const prompt = document.getElementById('prompt');
 (prompt as HTMLInputElement).addEventListener('keyup', (e) => {
 	if (e.key == 'Enter') {
 		handler((prompt as HTMLInputElement).value);
 	}
+});
+
+const send = document.getElementById('send');
+(send as HTMLButtonElement).addEventListener('click', () => {
+	handler((prompt as HTMLInputElement).value);
 });
