c412f1cf2a2c10af574395deb8a6e6925294ff50
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-03-03 10:14:01 +0000

    add clear to the help menu

--- src/public/scripts/commands/help.ts
+++ src/public/scripts/commands/help.ts
@@ -4,7 +4,8 @@ export default function help(): void {
 	new Line(
 		[new Phrase('white', `<br>
 			Here is a list of all commands.<br>
-			* help: prints this menu and exits.<br><br>
+			* help: prints this menu and exits.<br>
+			* clear: clears the terminal<br>
 		`)]
 	).write();
 }
