4b470e9ad2924d57db16070c5696e76d5f45e7e6
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-04-10 13:16:05 +0100

    refactor discord link

---
+++ src/scripts/discord.ts
@@ -0,0 +1,24 @@
+import copy from './copy';
+
+// eslint-disable-next-line import/prefer-default-export
+export function init() {
+  const discordAccount = '『EmeraldSnorlax』#8434';
+  const defaultText = 'You can find me online at:';
+
+  const discordButton = document.getElementById('discord');
+  const notifier = document.getElementById('account-copy-notifier')!;
+
+  discordButton!.addEventListener(('click'), () => {
+    copy(discordAccount, () => {
+      notifier.innerText = `${discordAccount} copied!`;
+    });
+  });
+
+  discordButton!.addEventListener(('mouseover'), () => {
+    notifier.innerText = `Click to copy ${discordAccount}`;
+  });
+
+  discordButton!.addEventListener(('mouseout'), () => {
+    notifier.innerText = defaultText;
+  });
+}
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,15 +1,3 @@
-import copy from './copy';
+import * as discord from './discord';
 
-const discordAccount = '『EmeraldSnorlax』#8434';
-
-const discordButton = document.getElementById('discord');
-
-discordButton!.addEventListener(('click'), () => {
-  copy(discordAccount, () => {
-    const notifier = document.getElementById('account-copy-notifier')!;
-    notifier.innerText = `${discordAccount} copied!`;
-    setTimeout(() => {
-      notifier.innerText = 'You can find me online at:';
-    }, 4000);
-  });
-});
+discord.init();
