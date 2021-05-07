281c1a3ba3c3f3d6161ebb2994edae1929e3ff2e
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-05-07 14:09:24 +0100

    svelte!

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,15 +1,17 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
   },
   plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
     [
       '@snowpack/plugin-typescript',
       {
         /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+        ...(process.versions.pnp ? {tsc: 'yarn pnpify tsc'} : {}),
       },
     ],
   ],
