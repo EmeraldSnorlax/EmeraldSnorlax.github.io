bbcd1adc5d93d5184d4e210d8b5c1fdfead1cfa4
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-05-09 14:55:18 +0100

    pagination

--- src/pages/projects.ts
+++ src/pages/projects.ts
@@ -3,8 +3,13 @@ import type Repository from './repository';
 function getProjects(user: string) {
   return new Promise<Repository[]>(async (resolve, reject) => {
     let res = await fetch(`https://api.github.com/users/${user}/repos`)
-    if (!res.ok) reject(res.statusText)
     let repos = await res.json();
+    let i = 2; // When the API starts counting pages from 1 :flushed:
+    while ( repos.length === 30 ) {
+      repos = [...repos, (await (await fetch(`https://api.github.com/users/${user}/repos?page=${i}`)).json())].flat(1)
+      i++;
+    }
+    if (!res.ok) reject(res.statusText)
     repos = repos.filter((repo: Repository) => !repo.fork && !repo.archived && repo.name !== user)
     resolve(repos);
   })
