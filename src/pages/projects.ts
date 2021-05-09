26d07a64ee97494ea167ef00c9e44af372355baa
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-05-09 15:14:56 +0100

    refactor github requests

--- src/pages/projects.ts
+++ src/pages/projects.ts
@@ -2,14 +2,16 @@ import type Repository from './repository';
 
 function getProjects(user: string) {
   return new Promise<Repository[]>(async (resolve, reject) => {
-    let res = await fetch(`https://api.github.com/users/${user}/repos`)
-    let repos = await res.json();
-    let i = 2; // When the API starts counting pages from 1 :flushed:
-    while ( repos.length === 30 ) {
-      repos = [...repos, (await (await fetch(`https://api.github.com/users/${user}/repos?page=${i}`)).json())].flat(1)
+    let repos: Repository[] = [];
+    let page: Repository[] = []
+    let i = 1; // When the API starts counting pages from 1 :flushed:
+    while ( page.length === 30 || repos.length === 0 ) {
+      let res = await fetch(`https://api.github.com/users/${user}/repos?page=${i}`);
+      page = await res.json();
+      if (!res.ok) reject(res.statusText);
+      repos = [...repos, page].flat(1);
       i++;
     }
-    if (!res.ok) reject(res.statusText)
     repos = repos.filter((repo: Repository) => !repo.fork && !repo.archived && repo.name !== user)
     resolve(repos);
   })
