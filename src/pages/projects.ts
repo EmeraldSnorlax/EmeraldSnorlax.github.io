e63d6a4287342e238b0c901f25f9d159036b3a5f
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-05-09 15:16:16 +0100

    semis

--- src/pages/projects.ts
+++ src/pages/projects.ts
@@ -3,7 +3,7 @@ import type Repository from './repository';
 function getProjects(user: string) {
   return new Promise<Repository[]>(async (resolve, reject) => {
     let repos: Repository[] = [];
-    let page: Repository[] = []
+    let page: Repository[] = [];
     let i = 1; // When the API starts counting pages from 1 :flushed:
     while ( page.length === 30 || repos.length === 0 ) {
       let res = await fetch(`https://api.github.com/users/${user}/repos?page=${i}`);
@@ -12,9 +12,9 @@ function getProjects(user: string) {
       repos = [...repos, page].flat(1);
       i++;
     }
-    repos = repos.filter((repo: Repository) => !repo.fork && !repo.archived && repo.name !== user)
+    repos = repos.filter((repo: Repository) => !repo.fork && !repo.archived && repo.name !== user);
     resolve(repos);
-  })
+  });
 }
 
 export default getProjects;
