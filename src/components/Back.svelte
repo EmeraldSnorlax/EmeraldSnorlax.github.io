ef57483c3ad7d0aabdb941bdda601247e69709a4
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-05-10 19:42:13 +0100

    increase readability of back button

--- src/components/Back.svelte
+++ src/components/Back.svelte
@@ -1,31 +1,38 @@
 <script lang="typescript">
-  import {
-    mdiChevronDoubleUp,
-    mdiChevronDoubleRight,
-    mdiChevronDoubleDown,
-    mdiChevronDoubleLeft,
-  } from "@mdi/js";
+  import { mdiHome } from "@mdi/js";
   import { location } from "../stores";
 </script>
 
-<button
-  on:click={() => $location = 'home'}
->
+<button on:click={() => ($location = "home")}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24px"
+    height="24px"><path d={mdiHome} /></svg
+  >
   Back
 </button>
 
 <style>
   button {
     border: none;
-    background: #424242;
-    color: white;
+    background: tomato;
+    color: black;
     padding: 1em;
     border-radius: 6px;
     transition: 0.2s;
     margin: 1em 0;
     width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
+
   button:hover {
     filter: brightness(0.8);
+  }
+
+  svg {
+    padding-right: 0.5em;
   }
 </style>
